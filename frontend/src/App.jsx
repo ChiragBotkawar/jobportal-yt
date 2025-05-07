517f14f9c6fcc86279d17dd4aa6297973818b386
chiragbotkawar123 <chiragbotkawar123@gmail.com>
2025-05-07 11:25:32 +0000

    Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/shared/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
-import Home from './components/Home'
 import Jobs from './components/Jobs'
-import Browse from './components/Browse'
+import PostJob from './components/admin/PostJob';
 import Profile from './components/Profile'
-import JobDescription from './components/JobDescription'
-import Companies from './components/admin/Companies'
-import CompanyCreate from './components/admin/CompanyCreate'
-import CompanySetup from './components/admin/CompanySetup'
-import AdminJobs from "./components/admin/AdminJobs";
-import PostJob from './components/admin/PostJob'
-import Applicants from './components/admin/Applicants'
-import ProtectedRoute from './components/admin/ProtectedRoute'
+import { Toaster } from './components/ui/sonner';
 
-
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
+function App() {
+  return (
+    <Router>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={<Home />} />
   {
     path: '/login',
     element: <Login />
@@ -34,51 +27,15 @@ const appRouter = createBrowserRouter([
     element: <Jobs />
   },
   {
-    path: "/description/:id",
-    element: <JobDescription />
-  },
-  {
-    path: "/browse",
-    element: <Browse />
-  },
-  {
-    path: "/profile",
-    element: <Profile />
-  },
-  // admin ke liye yha se start hoga
-  {
-    path:"/admin/companies",
-    element: <ProtectedRoute><Companies/></ProtectedRoute>
-  },
-  {
-    path:"/admin/companies/create",
-    element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/companies/:id",
-    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
-  },
-  {
-    path:"/admin/jobs",
-    element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
-  },
-  {
     path:"/admin/jobs/create",
     element:<ProtectedRoute><PostJob/></ProtectedRoute> 
   },
-  {
-    path:"/admin/jobs/:id/applicants",
-    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
-  },
 
-])
-function App() {
-
-  return (
-    <div>
-      <RouterProvider router={appRouter} />
-    </div>
-  )
+        <Route path='/admin/post-job' element={<PostJob />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
